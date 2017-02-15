37d36545a9e004d93aa8175d13301057c104119b
zhibingzhang <466163792@qq.com>
2017-02-15 17:21:37 +0800

    update node+mongodb

--- nodeweb/app.js
+++ nodeweb/app.js
@@ -1,12 +1,20 @@
 var express = require('express')
+var path = require('path')
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+var _= require('underscore')
+var Movie = require('./models/movie.js')
 var port = process.env.PORT || 3000
 var app = express()
 
-//定义public路径
-var pub = __dirname + '/public';
-app.use(express.static( __dirname + '/public'));//设置静态目录为pubic
-app.set('views','views')
+
+mongoose.connect('mongodb://localhost/nodeweb')
+
+app.set('views','views/pages')
 app.set('view engine','jade')//设置默认的模板引擎
+//定义public路径
+app.use(express.static(path.join(__dirname, 'public')))//设置静态目录为pubic
+app.use(bodyParser.urlencoded({ extended: false }))
 app.listen(port)
 
 
@@ -15,25 +23,106 @@ console.log('nodeweb started on port '+port)
 
 // 页面路由 index page
 app.get('/',function(req, res){
-	res.render('pages/index',{
-		title: 'nodeweb 首页'
+	Movie.fetch(function(err, movies){
+		if(err){
+			console.log(err)
+		}
+		res.render('index',{
+			title: 'nodeweb 首页',
+			movies: movies
+		})
 	})
 })
-// // 页面路由 detail page
-// app.get('/',function(req, res){
-// 	res.render('detail',{
-// 		title: 'nodeweb 详情页'
+// // // 页面路由 detail page
+// app.get('/movie/:id',function(req, res){
+// 	var id = req.params.id
+
+// 	Movie.findById(id, function(err, movie){
+// 		res.render('detail',{
+// 			title: 'nodeweb 详情页'+ movie.title,
+// 			movies: movie
+// 		})
 // 	})
 // })
-// // 页面路由 admin page
+// // // 页面路由 admin page
 // app.get('/admin/movie',function(req, res){
 // 	res.render('admin',{
-// 		title: 'nodeweb 后台录入页'
+// 		title: 'nodeweb 后台录入页',
+// 		movie: {
+// 			title: '',
+// 			doctor: '',
+// 			country: '',
+// 			year: '',
+// 			poster: '',
+// 			flash: '',
+// 			summary: '',
+// 			language: ''
+// 		}
 // 	})
+// })
+// //admin update movid
+// app.get('/admin/update/:id', function(req, res){
+// 	var id = req.params.id
+
+// 	if(id){
+// 		Movie.findById(id, function(err, movie){
+// 			res.render('admin', {
+// 				title: 'nodeweb 后台更新页',
+// 				movie: movie
+// 			})
+// 		})
+// 	}
+// })
+// //admin post movie
+// app.post('/admin/movie/new', function(res, req){
+// 	var id = req.body.movie._id
+// 	var movieObj = req.body.movie
+// 	var _movie
+
+// 	if(id !== 'undefined'){
+// 		Movie.findById(id, function(err, movie){
+// 			if(err){
+// 				console.log(err)
+// 			}
+
+// 			_movie = _.extend(movie, movieObj)
+// 			_movie.save(function(err, movie){
+// 				if(err){
+// 					console.log(err)
+// 				}
+// 				res.redirect('/movie' +movie._id)
+// 			})
+// 		})
+// 	}else{
+// 		_movie = new Movie({
+// 			doctor: movieObj.doctor,
+// 			title: movieObj.title,
+// 			country: movieObj.country,
+// 			language: movieObj.language,
+// 			year: movieObj.year,
+// 			poster: movieObj.poster,
+// 			summary: movieObj.summary,
+// 			flash: movieObj.flash
+// 		})
+
+// 		_movie.save(function(err, movie){
+// 			if(err){
+// 					console.log(err)
+// 				}
+// 				res.redirect('/movie' +movie._id)
+// 		})
+// 	}
+
 // })
 // // 页面路由 list page
 // app.get('/admin/list',function(req, res){
-// 	res.render('list',{
-// 		title: 'nodeweb 列表页'
+// 	Movie.fetch(function(err, movies){
+// 		if(err){
+// 			console.log(err)
+// 		}
+// 		res.render('list',{
+// 			title: 'nodeweb 列表页',
+// 			movies: movies
+// 		})
 // 	})
 // })
